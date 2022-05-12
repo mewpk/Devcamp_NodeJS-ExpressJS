6fd6a7d4f761ba0ab9dc8aeebf53df823380a249
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-12 13:29:15 +0700

    save

--- Lap3/app.js
+++ Lap3/app.js
@@ -36,6 +36,6 @@ app.get('/api/populations' , (req, res) => {
      ]);
     });
 
-app.listen(3000, () => {
-  console.log("server started on port 3000!");
+app.listen(3001, () => {
+  console.log("server started on port 3001!");
 });
---
+++ lap4/src/App.js
@@ -0,0 +1,29 @@
+import { Table } from 'antd';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+export default function App() {
+  const columns = [
+    {
+      title: 'State',
+      dataIndex: 'State',
+      key: 'State',
+    },
+    {
+      title: 'Year',
+      dataIndex: 'Year',
+      key: 'Year',
+    },
+    {
+      title: 'Population',
+      dataIndex: 'Population',
+      key: 'Population',
+    },
+  ];
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('/api/populations').then((result) => {
+      setData(result.data);
+    });
+  }, []);
+  return <Table dataSource={data} columns={columns} pagination={false} />;
+}
